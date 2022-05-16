2792a711701ea35a512afb779134bb0f580020da
Thomas Ricketts <rickettsdev@gmail.com>
2022-05-16 12:29:25 -0400

    Replaced react-router dependency with react-router-dom, and added intial router code.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
+import { Index, Product } from './Pages';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <BrowserRouter>
+      <Routes>
+      <Route path="/"  element={<Index />}/>
+      <Route path="/rooms/new" element={<Product />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Pages.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Product = () => <h1>Product Page</h1>;
+export const Index = () => <h1>Index Page</h1>;
+export const Bar = () => <h1>Bar Page</h1>;
